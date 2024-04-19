0b08f686eae37341fa04eaf32c77aea2497e9a2b
Anna Hill <Anna.Hill@students.jmcss.org>
2024-04-19 08:44:30 -0500

    wwww

--- includes/header.php
+++ includes/header.php
@@ -1,41 +1,39 @@
-!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PowerFoods</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Rampart+One&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">PowerFoods</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="products.php">Products</a>
-            <a class="nav-link" href="contact.php">Contact Us</a>
-            <a class="nav-link" href="signup.php">Sign Up!</a>
-          </div>
-        </div>
-      </div>
-    </nav>
-    <!DOCTYPE html>
+  <!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title></title>
+      <link rel="stylesheet" href="style.css">
     </head>
     <body>
-      
-
+      <header>
+        <nav class="nav-header-main">
+        <a class="header.logo" href="index.php">
+          <img src="img/removebg-preview.jpg" alt="logo">
+        </a>
+        <ul> 
+          <li><a href="index.php">Home</a></li>
+          <li><a href="products.php">Products</a></li>
+          <li><a href="quantity.php">Quantity</a></li>
+          <li><a href="contact.php">Contact</a></li>
+          <li><a href="signup.php">Signup</a></li>
+          <li><a href="checkout.php">Checkout</a></li>
+        </ul>
+      <div>
+  <form action="includes/login.inc.php" method="post"> 
+    <input type="text" name="mailuid" placeholder="Username/E-mail...">
+    <input type="password" name="pwd" placeholder="Password...">
+    <button type="submit" name="Login-submit">Login</button>
+  </form>
+  <a href="signup.php">Signup</a>
+  <form action="includes/logout.inc.php" method="post"> 
+  
+    <button type="submit" name="Logout-submit">Logout</button>
+  </form>
+</div>
+</nav>
+ </header>
 
 
 
--- index.php
+++ index.php
@@ -31,6 +31,23 @@
 
         <center><a href="products.html" class="btn product">Shop With Us!</a></center>
   </div>
+
+  <?php
+require "header.php";
+?>
+
+          <main>
+            <div class="wrapper-main">
+              <section class="section-default"></section>
+                 <p class="login-status">You are logged out!</p>
+                 <p class="login-status">You are logged in!</p>
+            </div>
+          </main>
+
+
+
+
+
   <footer class="bg-body-tertiary text-center">
     <!-- Grid container -->
     <div class="container p-4 pb-0">
@@ -104,7 +121,7 @@
     <a class="text-body" href="https://mdbootstrap.com/">MDBootstrap.com</a>
   </div>
   <?php
-
+ 
       $sql = "SELECT * FROM users;";
       $result = mysqli_query($conn, $sql);
       $resultCheck = mysqli_num_rows($result);
